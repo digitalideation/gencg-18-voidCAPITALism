b38f098443fc31734d7a2a06f35038137986aa0c
Marlon <marlonstadler@gmx.ch>
2018-10-31 13:59:47 +0100

    changed to p5 min

--- _04-Noise/01_Plane/sketch.js
+++ _04-Noise/01_Plane/sketch.js
@@ -1,7 +1,10 @@
 // Global var
-let points = [];
-let center;
-let radius = 500;
+let numberOfCircles = 10;
+let noiseFactor = 100;
+let colorSpeed = 0;
+let circles = [];
+let drawLines = false;
+
 function setup() {
   // Canvas setup
   canvas = createCanvas(windowWidth, windowHeight);
@@ -9,37 +12,81 @@ function setup() {
   // Detect screen density (retina)
   var density = displayDensity();
   pixelDensity(density);
+
+  p5.disableFriendlyErrors = true
+
+  colorMode(HSB);
   background(0);
-  stroke(255);
   noFill();
-  center = createVector(windowWidth/2,windowHeight/2);
+
+  initCircle();
 }
+let tick=0;
 function draw() {
   background(0);
-
-  noiseSeed(random());
-  for(let i=0;i<360;i++){
-    points.push(pointCoordinates(center,radius,i));
+  noiseFactor = map(mouseX,0,width,0,300,true);
+  colorSpeed = floor(map(mouseY,0,height,0,100,true));
+  push()
+  translate(-noiseFactor/2,0);
+  for(let i = 0;i<numberOfCircles;i++){
+    drawCircle(circles[i],tick)
   }
-  for(let i=0;i<points.length;i++){
-    points[i].x = points[i].x + noise(points[i].x,points[i].y)*100;
-    points[i].y = points[i].y + noise(points[i].x,points[i].y)*100;
-  }
-
-
-  for(let i = 0;i<points.length-1;i++){
-
-    ellipse(points[i].x,points[i].y,1,1);
-    line(points[i].x,points[i].y,points[i+1].x,points[i+1].y)
-  }
+  pop()
+  tick++;
 }
 
 function keyPressed() {
   if (key == 't' || key == 'T') {saveThumb(650, 350)}
-  if (key == 's' || key == 'S') {savePic(windowWidth,windowHeight)}
+  if (key == 's' || key == 'S') {savePic(width,height)}
 }
 
 // Tools
+function drawCircle(points,tick){
+  for(let i = 0;i<points.length-1;i++){
+    let string = 'hsb('+((i+(tick*colorSpeed))%360)+', 100%, 100%)'
+    c = color(string);
+    stroke(c);
+    ellipse(
+      points[i].x + noise(points[i].x,points[i].y,tick)*noiseFactor,
+      points[i].y,
+      3,3
+    );
+    if(drawLines){
+      line(
+        points[i].x + noise(points[i].x,points[i].y,tick)*noiseFactor,
+        points[i].y,
+        points[i+1].x + noise(points[i+1].x,points[i+1].y,tick)*noiseFactor,
+        points[i+1].y,
+      );
+    }
+  }
+  if(drawLines){
+    line(points[0].x + noise(points[0].x,points[0].y,tick)*noiseFactor,
+      points[0].y,
+      points[points.length-1].x + noise(points[points.length-1].x,points[points.length-1].y,tick)*noiseFactor,
+      points[points.length-1].y
+    );
+  }
+}
+
+function initCircle(){
+  circles = [];
+  let center = createVector(width/2,height/2);
+  let radiusStart = height/2;
+
+  for(let i = 0;i<numberOfCircles;i++){
+    circles.push(circlePoints(center,radiusStart-i*(height/2/numberOfCircles)));
+  }
+}
+
+function circlePoints(center,radius){
+  let points = [];
+  for(let i=0;i<360+1;i++){
+    points.push(pointCoordinates(center,radius,i));
+  }
+  return points;
+}
+
 function pointCoordinates(center,r,angle){
   t = toRadians(angle);
   let x = r*cos(t) + center.x;
@@ -50,9 +97,15 @@ function pointCoordinates(center,r,angle){
 function toRadians (angle) {
   return angle * (Math.PI / 180);
 }
+
+function mousePressed(){
+  drawLines = !drawLines;
+}
+
 // resize canvas when the window is resized
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight, false);
+  initCircle();
 }
 
 // Int conversion
